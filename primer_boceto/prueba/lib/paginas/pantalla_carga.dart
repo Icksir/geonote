b2c0c2b43a5fee1d2a23a2c8b69cc3507d1d79e3
caverav <camilo.verav@sansano.usm.cl>
2020-12-07 22:25:54 -0300

    pequeños arreglos

--- primer_boceto/prueba/lib/paginas/pantalla_carga.dart
+++ primer_boceto/prueba/lib/paginas/pantalla_carga.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:async';
+import 'dart:core';
+
+import 'package:prueba/paginas/home.dart';
 
 class Carga extends StatefulWidget {
   @override
@@ -6,8 +13,34 @@ class Carga extends StatefulWidget {
 }
 
 class _CargaState extends State<Carga> {
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    List datos;
+    Future<void> getDatos() async {
+      http.Response response = await http.get(
+          Uri.encodeFull("https://api.gael.cl/general/public/sismos"),
+          headers: {"Accept": "application/json"});
+      setState(() {
+        datos = json.decode(response.body);
+      });
+    }
+    while (datos==null) {
+      getDatos();
+    }
+    Navigator.of(context).push(MaterialPageRoute<Null>(
+        builder: (BuildContext context){
+          return new Home(datos);
+        }
+    ));
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: SpinKitCircle(
+          color: Color(0xff3f51b5),
+          size: 80.0,
+        ),
+      ),
+    );
   }
 }
