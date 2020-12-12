0f10aaef7e59be8667c21cef960b6615393a3866
Ricky <ricardo.olalquiaga@sansano.usm.cl>
2020-12-12 12:29:05 -0300

    datos

--- primer_boceto/prueba/lib/paginas/config.dart
+++ primer_boceto/prueba/lib/paginas/config.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Config extends StatefulWidget {
   @override
@@ -6,13 +7,69 @@ class Config extends StatefulWidget {
 }
 
 class _ConfigState extends State<Config> {
+  var _lista = ["Santiago", "Valparaiso", "Vina del Mar", "Talca"];
+  String _menu = "Seleccione una opcion:";
+  bool _checkBoxValue = false;
+
+  Future<Null> saveOption(bool isSelected) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setBool('option', isSelected);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blueAccent,
-        title: Text('Configuración'),
-      ),
-    );
+        appBar: AppBar(
+          backgroundColor: Colors.blueAccent,
+          title: Text('Configuración'),
+        ),
+        body: ListView(
+          children: <Widget>[
+            SwitchListTile(
+                title: Text("¿Quieres recibir notificaciones de los sismos?",
+                    style: TextStyle(fontSize: 17)),
+                secondary: Icon(Icons.notification_important_outlined),
+                controlAffinity: ListTileControlAffinity.platform,
+                value: _checkBoxValue,
+                onChanged: (bool value) {
+                  setState(() {
+                    _checkBoxValue = value;
+                  });
+                }),
+            DropdownButton(
+                items: _lista.map((String bbb) {
+                  return DropdownMenuItem(value: bbb, child: Text(bbb));
+                }).toList(),
+                onChanged: (_value) => {
+                      setState(() {
+                        _menu = _value;
+                      })
+                    },
+                hint: Text(_menu))
+          ],
+        )
+        /*Container(
+            child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(10.0, 5.0, 4.0, 5.0),
+                  child: Text("¿Quieres recibir notificaciones de los sismos?",
+                      style: TextStyle(fontSize: 17)),
+                ),
+                Checkbox(
+                    value: checkBoxValue,
+                    onChanged: (bool value) {
+                      setState(() {
+                        checkBoxValue = value;
+                        print(value);
+                      });
+                    }),
+              ],
+            )
+          ],
+        )) )*/
+        );
   }
 }
---
+++ primer_boceto/prueba/lib/servicios/saved_data.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class UserPreferences {
+  static final UserPreferences _instance = UserPreferences._ctor();
+  factory UserPreferences() {
+    return _instance;
+  }
+
+  UserPreferences._ctor();
+}
