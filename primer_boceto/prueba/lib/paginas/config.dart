d3e60ee5b5da963d26c155dcb5c23c0d079db879
iTzGooDLife <julio.cerdav@sansano.usm.cl>
2020-12-21 21:02:46 -0300

    ciudades

--- primer_boceto/prueba/lib/paginas/config.dart
+++ primer_boceto/prueba/lib/paginas/config.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:prueba/servicios/preferencias.dart';
+import 'package:smart_select/smart_select.dart';
 
 class Config extends StatefulWidget {
   @override
@@ -11,6 +12,14 @@ class _ConfigState extends State<Config> {
   String _menu = "Seleccione una opcion:"; */
   Preferencias _preferencias = Preferencias();
   /* bool _checkBoxValue = false; */
+
+  List<String> value1 = [];
+  List<S2Choice<String>> ciudades1 = [
+    S2Choice<String>(value: "1", title: 'Santiago'),
+    S2Choice<String>(value: "2", title: 'Viña del mar'),
+    S2Choice<String>(value: "3", title: 'Valparaíso'),
+    S2Choice<String>(value: "4", title: 'Talca (aunque no exista)'),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -33,7 +42,27 @@ class _ConfigState extends State<Config> {
                   _preferencias.automatico = value;
                   _preferencias.commit();
                 });
-              }), /* ,
+              }),
+          SmartSelect<String>.multiple(
+              title: 'Ciudades de preferencia',
+              value: _preferencias.ciudades,
+              choiceItems: ciudades1,
+              onChange: (state) {
+                setState(
+                  () {
+                    value1 = state.value;
+                    _preferencias.ciudades = value1;
+                    _preferencias.commit();
+                  },
+                );
+              })
+
+          /* SmartSelect<String>.multiple(
+              title: 'Frameworks',
+              value: value,
+              choiceItems: options,
+              onChange: (state) => setState(() => value = state.value)) */
+          /* ,
             DropdownButton(
                 items: _lista.map((String bbb) {
                   return DropdownMenuItem(value: bbb, child: Text(bbb));
@@ -46,28 +75,6 @@ class _ConfigState extends State<Config> {
                 hint: Text(_menu)) */
         ],
       ),
-      /*Container(
-            child: Column(
-          children: <Widget>[
-            Row(
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(10.0, 5.0, 4.0, 5.0),
-                  child: Text("¿Quieres recibir notificaciones de los sismos?",
-                      style: TextStyle(fontSize: 17)),
-                ),
-                Checkbox(
-                    value: checkBoxValue,
-                    onChanged: (bool value) {
-                      setState(() {
-                        checkBoxValue = value;
-                        print(value);
-                      });
-                    }),
-              ],
-            )
-          ],
-        )) )*/
     );
   }
 
--- primer_boceto/prueba/lib/servicios/preferencias.dart
+++ primer_boceto/prueba/lib/servicios/preferencias.dart
@@ -2,12 +2,29 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class Preferencias {
   static const AUTOMATICO = "automatico";
+  static const CIUDADES = "ciudades";
+  //
+  /* static const SANTIAGO = "santiago";
+  static const VINA = "vina";
+  static const VALPARAISO = "valparaiso";
+  static const TALCA = "talca"; */
 
   static final Preferencias instance = Preferencias._internal();
-
-  //campos
+  //
+  /* static final Preferencias instance1 = Preferencias._internal();
+  static final Preferencias instance1 = Preferencias._internal();
+  static final Preferencias instance1 = Preferencias._internal();
+  static final Preferencias instance1 = Preferencias._internal();   */
+  //
   SharedPreferences _sharedPreferences;
+  //campos
+  List ciudades = [];
+
   bool automatico = false;
+/*   bool santiago = false;
+  bool vina = false;
+  bool valparaiso = false;
+  bool talca = false; */
 
   Preferencias._internal();
 
@@ -19,16 +36,45 @@ class Preferencias {
     } else {
       _sharedPreferences = await SharedPreferences.getInstance();
       automatico = _sharedPreferences.getBool(AUTOMATICO);
+      ciudades = _sharedPreferences.getStringList(CIUDADES);
+      /* santiago = _sharedPreferences.getBool(SANTIAGO);
+      vina = _sharedPreferences.getBool(VINA);
+      valparaiso = _sharedPreferences.getBool(VALPARAISO);
+      talca = _sharedPreferences.getBool(TALCA); */
       if (automatico == null) {
         automatico = false;
+        /* santiago = false;
+        vina = false;
+        valparaiso = false;
+        talca = false; */
+      }
+      if (ciudades == null) {
+        ciudades = [];
       }
       return _sharedPreferences;
     }
   }
 
+/*   Future<SharedPreferences> get ciudades async {
+    if (_sharedPreferences != null) {
+      return _sharedPreferences;
+    } else {
+      _sharedPreferences = await SharedPreferences.getInstance();
+      automatico = _sharedPreferences.getBool(AUTOMATICO);
+      if (automatico == null) {
+        automatico = false;
+      }
+      return _sharedPreferences;
+    }
+  } */
 //guardar
   Future<bool> commit() async {
     _sharedPreferences.setBool(AUTOMATICO, automatico);
+    _sharedPreferences.setStringList(CIUDADES, ciudades);
+    /* _sharedPreferences.setBool(SANTIAGO, santiago);
+    _sharedPreferences.setBool(VINA, vina);
+    _sharedPreferences.setBool(VALPARAISO, valparaiso);
+    _sharedPreferences.setBool(TALCA, talca); */
   }
 
 //leer
